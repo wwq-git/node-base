6bbf1da4de44da88266aa92ca50cfe867bcf3b47
wwq-git <wwq921220@163.com>
2018-05-07 13:54:41 +0800

    nodejs5_读取文件(n5_readfile)

---
+++ models/optfile.js
@@ -0,0 +1,19 @@
+var  fs=  require('fs');
+module.exports={
+  readfile:function(path){    //异步执行
+    fs.readFile(path,  function  (err,  data)  {
+    if  (err)  {
+      console.log(err);
+    }else{
+      console.log(data.toString());
+    }
+    });
+    console.log("异步方法执行完毕");
+  },
+  readfileSync:function(path){  //同步读取
+    var  data  =  fs.readFileSync(path,'utf-8');
+    console.log(data.toString());
+    console.log("同步方法执行完毕");
+    return  data;      
+  }
+} 
